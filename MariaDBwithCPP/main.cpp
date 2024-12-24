b29eeabd9a21c3d612b6e8214f5fafbb21ce8e9a
ErixTheUncoder <erichong.0969@gmail.com>
2024-12-24 17:14:20 +0800

    test for branch

--- MariaDBwithCPP/main.cpp
+++ MariaDBwithCPP/main.cpp
@@ -10,6 +10,12 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    test
+    //testi
     return 0;
 }
+
+
+//Create database function
+void functionCreateDB(){
+    
+};
